5cf8b2d392f3bacb99b2f61f1d6b62cd8b55cfdd
RoberthLoorDev <roberthloor.dev@gmail.com>
2026-06-21 17:20:58 -0500

    fix: update metro.config.js to enable symlinks and package exports

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
+config.watchFolders = [path.resolve(__dirname, '../../node_modules')];
+
+module.exports = withNativeWind(config, { input: './src/global.css' });
